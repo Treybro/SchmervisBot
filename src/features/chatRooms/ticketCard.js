cddecb2b98cade12d3ec2d5905d8fdd0805dcdd2
Hyper Mediocrity <michael.treyvaud@gmail.com>
2017-06-13 22:32:39 +0100

    Changed banner style for smaller phones

--- src/features/chatRooms/ticketCard.js
+++ src/features/chatRooms/ticketCard.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
 
   containerView: {
     backgroundColor: theme.blue,
-    height: 150,
+    height: 120,
     justifyContent: 'center',
     alignItems: 'center',
   },
